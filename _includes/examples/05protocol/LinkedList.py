06894234cfb50efee7c205b0b0e54aca9db4c2b8
Scott Irwin <sirwin@rocketmail.com>
2019-06-13 14:03:15 -0400

    inherit from collections.abc.MutableSequence; move tests to seperate py file

--- _includes/examples/05protocol/LinkedList.py
+++ _includes/examples/05protocol/LinkedList.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Any
+from collections.abc import MutableSequence
 
 @dataclass
 class Node:
@@ -21,14 +22,14 @@ class LListIterator:
             self._iter_pos = cur.next
             return cur.data
 
-class LinkedList:
+class LinkedList(MutableSequence):
     def __init__(self):
         self._num_nodes = 0
         self.head = None
         self.tail = None
 
-    def append(self, x):
-        e = Node(x, next=None)
+    def append(self, value):
+        e = Node(value, next=None)
         if self.head is None:
             self.head = e
         else:
@@ -36,8 +37,8 @@ class LinkedList:
         self.tail = e
         self._num_nodes += 1
 
-    def prepend(self, x):
-        e = Node(x, next=self.head)
+    def prepend(self, value):
+        e = Node(value, next=self.head)
         self.head = e
         if self.tail is None:
             self.tail = e
@@ -54,58 +55,97 @@ class LinkedList:
             curr = nxt
         self.head = prev
 
-    def __iter__(self):
-        return LListIterator(self)
-
-    def _get(self, index):
-        pos = 0
-        ret = self.head
-        while (pos < index) and (ret is not None):
-            ret = ret.next
-            pos += 1
-        return ret.data
-
-    def __getitem__(self, index):
-        if isinstance(index,int):
-            if 0 <= index < self._num_nodes:
-                return self._get(index)
-            else:
-                raise IndexError
-        else:
-            raise TypeError(f'{self.__class__.__qualname__}.__getitem__ index({index!r}) must be int')
-
-    def __contains__(self, item):
-        for val in self:
-            if val == item:
-                return True
-        return False
+    def insert(self, index, value):
+        if self._index_ok(index):
+            return self._ins(index, value)
 
     def __len__(self):
         return self._num_nodes
 
     def __repr__(self):
-        return ','.join([str(val) for val in self])
+        contents_str = ','.join([f'{x!r}' for x in self])
+        return f'{self.__class__.__qualname__}[{contents_str}]'
 
-def main():
-    llist = LinkedList()
-    for i in range(10):
-        llist.append(i)
-    print(f'llist = {llist!r}')
+    def __getitem__(self, i):
+        if self._index_ok(i):
+            return self._get(i)
 
-    for i in range(len(llist)):
-        print(llist[i])
+    def __setitem__(self, i, x):
+        if self._index_ok(i):
+            return self._set(i, x)
 
-    print(f'llist[2] = {llist[2]!r}')
+    def __delitem__(self, i):
+        if self._index_ok(i):
+            return self._del(i)
 
-    print(f' 5 is in llist: {5 in llist}')
-    print(f'42 is in llist: {42 in llist}')
+    def __contains__(self, x):
+        for val in self:
+            if val == x:
+                return True
+        return False
 
-    for i in range(10,20):
-        llist.prepend(i)
-    print(f'llist = {llist!r}')
+    def __iter__(self):
+        return LListIterator(self)
 
-    llist.reverse()
-    print(f'reversed llist = {llist!r}')
+    # prevent reverse itertion
+    __reversed__ = None
 
-if __name__ == '__main__':
-    main()
+    def _index_ok(self, i):
+        if isinstance(i,int):
+            if 0 <= i < self._num_nodes:
+                return True
+            else:
+                raise IndexError
+        else:
+            raise TypeError(f'{self.__class__.__qualname__} index {i!r} must be int')
+
+    def _get(self, i):
+        pos = 0
+        ret = self.head
+        while (pos < i):
+            ret = ret.next
+            pos += 1
+        return ret.data
+
+    def _set(self, i, x):
+        pos = 0
+        c = self.head
+        while (pos < i):
+            c = c.next
+            pos += 1
+        c.data = x
+
+    def _del(self, i):
+        if i == 0:
+            cur_head = self.head
+            self.head = cur_head.next
+            cur_head.next = None
+        else:
+            p = self.head
+            c = p.next
+            pos = 1
+            while pos < i:
+                p = c
+                c = c.next
+                pos += 1
+            p.next = c.next
+            c.next = None
+            if c == self.tail:
+                self.tail = p
+        self._num_nodes -= 1
+
+    def _ins(self, i, x):
+        if i == 0:
+            e = Node(x, next=self.head)
+            self.head = e
+        else:            
+            p = self.head
+            c = p.next
+            pos = 1
+            while pos < i:
+                p = c
+                c = c.next
+                pos += 1
+            e = Node(x, next=c)
+            p.next = e
+        self._num_nodes += 1
---
+++ _includes/examples/05protocol/test_linkedlist.py
@@ -0,0 +1,124 @@
+import unittest
+
+from LinkedList import LinkedList
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_01_repr_empty_list(self):
+        llist = LinkedList()
+        self.assertEqual('LinkedList[]', str(llist))
+
+    def test_02_append(self):
+        llist = LinkedList()
+        self.assertEqual('LinkedList[]', str(llist))
+        for i in range(10):
+            llist.append(i*2)
+        self.assertEqual('LinkedList[0,2,4,6,8,10,12,14,16,18]', str(llist))
+
+    def test_03_len(self):
+        sz = 10
+        llist = LinkedList()
+        self.assertEqual(0, len(llist))
+        for i in range(sz):
+            llist.append(i*2)
+        self.assertEqual(sz, len(llist))
+        llist.append('spam')
+        self.assertEqual(sz+1, len(llist))
+
+    def test_04_getitem(self):
+        size = 10
+        llist = LinkedList()
+        for i in range(size):
+            llist.append(i*2)
+        self.assertEqual((4, 6, 10), (llist[2], llist[3], llist[5]))
+        with self.assertRaises(TypeError):
+            llist['a']
+        with self.assertRaises(IndexError):
+            llist[size]
+        with self.assertRaises(IndexError):
+            llist[-1]
+
+    def test_05_contains(self):
+        sz = 10
+        llist = LinkedList()
+        for i in range(sz):
+            llist.append(i*2)
+        self.assertTrue(2 in llist)
+        self.assertFalse(42 in llist)
+
+    def test_06_setitem(self):
+        sz = 10
+        llist = LinkedList()
+        for i in range(sz):
+            llist.append(i*2)
+        llist[0] = '-28'
+        self.assertEqual("LinkedList['-28',2,4,6,8,10,12,14,16,18]", str(llist))
+        llist[sz-1] = 42
+        self.assertEqual("LinkedList['-28',2,4,6,8,10,12,14,16,42]", str(llist))
+        llist[sz//2] = -sz
+        self.assertEqual("LinkedList['-28',2,4,6,8,-10,12,14,16,42]", str(llist))
+        self.assertTrue(42 in llist)
+        with self.assertRaises(TypeError):
+            llist['a'] = 'a'
+        with self.assertRaises(IndexError):
+            llist[sz] = sz
+        with self.assertRaises(IndexError):
+            llist[-1] = -1
+
+    def test_07_delitem(self):
+        sz = 10
+        llist = LinkedList()
+        for i in range(sz):
+            llist.append(i*2)
+        del llist[sz-1]
+        self.assertEqual("LinkedList[0,2,4,6,8,10,12,14,16]", str(llist))
+        del llist[0]
+        self.assertEqual("LinkedList[2,4,6,8,10,12,14,16]", str(llist))
+        del llist[3]
+        self.assertEqual("LinkedList[2,4,6,10,12,14,16]", str(llist))
+
+    def test_08_prepend(self):
+        sz = 5
+        llist = LinkedList()
+        for i in range(sz):
+            llist.append(i*2)
+        for i in range(len(llist),len(llist)+sz):
+            llist.prepend(i*3)
+        self.assertEqual("LinkedList[27,24,21,18,15,0,2,4,6,8]", str(llist))
+        llist = LinkedList()
+        for i in range(sz):
+            llist.prepend(i*2)
+        for i in range(len(llist),len(llist)+sz):
+            llist.append(i*3)
+        self.assertEqual("LinkedList[8,6,4,2,0,15,18,21,24,27]", str(llist))
+
+    def test_09_insert(self):
+        sz = 5
+        llist = LinkedList()
+        for i in range(sz):
+            llist.append(i*2)
+        llist.insert(0, 'spam')
+        self.assertEqual("LinkedList['spam',0,2,4,6,8]", str(llist))
+        llist.insert(len(llist)-1, 'spam')
+        self.assertEqual("LinkedList['spam',0,2,4,6,'spam',8]", str(llist))
+        llist.insert(3, 'spam')
+        self.assertEqual("LinkedList['spam',0,2,'spam',4,6,'spam',8]", str(llist))
+        with self.assertRaises(TypeError):
+            llist.insert('a', 'spam')
+        with self.assertRaises(IndexError):
+            llist.insert(len(llist), 'spam')
+        with self.assertRaises(IndexError):
+            llist.insert(-1, 'spam')
+
+    def test_10_reverse(self):
+        sz = 5
+        llist = LinkedList()
+        for i in range(sz):
+            llist.append(i*2)
+        for i in range(len(llist),len(llist)+sz):
+            llist.prepend(i*3)
+        llist.reverse()
+        self.assertEqual("LinkedList[8,6,4,2,0,15,18,21,24,27]", str(llist))
+
+if __name__ == '__main__':
+    unittest.main()
